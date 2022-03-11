805126f86152c5d0ec5a528b68f1c4a53ba74877
Monica Stirbescu <monicastirbescu9@gmail.com>
2022-03-11 19:27:35 +0200

    CTS

---
+++ Seminar_2/src/Elev.java
@@ -0,0 +1,25 @@
+
+
+public class Elev extends Aplicant {
+    private int clasa;
+    private String tutore;
+
+    public void setClasa(int i) {
+        this.clasa = i;
+    }
+
+    public void setTutore(String tutore) {
+        this.tutore = tutore;
+    }
+
+    @Override
+    public String toString() {
+        return "Elev: "+super.toString() + clasa + ", Tutore=" + tutore;
+    }
+
+    public Elev() {
+        super();
+    }
+
+
+}
